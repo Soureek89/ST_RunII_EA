d1e6384a5a870610c4e701e5cec59f89b1a3d55b
Nils Faltermann <nils.faltermann@cern.ch>
2015-08-19 20:13:40 +0200

    latest version, old jetID

--- scripts/copy_data_prompt.sh
+++ scripts/copy_data_prompt.sh
@@ -1,42 +1,42 @@
 echo '### 1 ###'
-gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150804_115350/0000/treesTest_NewSmall_1.root file:./SingleMuon_1.root
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150813_133714/0000/treesTest_NewSmall_1.root file:./SingleMuon_1.root
 echo '### 2 ###'
-gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150804_115350/0000/treesTest_NewSmall_2.root file:./SingleMuon_2.root
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150813_133714/0000/treesTest_NewSmall_2.root file:./SingleMuon_2.root
 echo '### 3 ###'
-gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150804_115350/0000/treesTest_NewSmall_3.root file:./SingleMuon_3.root
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150813_133714/0000/treesTest_NewSmall_3.root file:./SingleMuon_3.root
 echo '### 4 ###'
-gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150804_115350/0000/treesTest_NewSmall_4.root file:./SingleMuon_4.root
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150813_133714/0000/treesTest_NewSmall_4.root file:./SingleMuon_4.root
 echo '### 5 ###'
-gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150804_115350/0000/treesTest_NewSmall_5.root file:./SingleMuon_5.root
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150813_133714/0000/treesTest_NewSmall_5.root file:./SingleMuon_5.root
 echo '### 6 ###'
-gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150804_115350/0000/treesTest_NewSmall_6.root file:./SingleMuon_6.root
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150813_133714/0000/treesTest_NewSmall_6.root file:./SingleMuon_6.root
 echo '### 7 ###'
-gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150804_115350/0000/treesTest_NewSmall_7.root file:./SingleMuon_7.root
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150813_133714/0000/treesTest_NewSmall_7.root file:./SingleMuon_7.root
 echo '### 8 ###'
-gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150804_115350/0000/treesTest_NewSmall_8.root file:./SingleMuon_8.root
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150813_133714/0000/treesTest_NewSmall_8.root file:./SingleMuon_8.root
 echo '### 9 ###'
-gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150804_115350/0000/treesTest_NewSmall_9.root file:./SingleMuon_9.root
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150813_133714/0000/treesTest_NewSmall_9.root file:./SingleMuon_9.root
 echo '### 10 ###'
-gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150804_115350/0000/treesTest_NewSmall_10.root file:./SingleMuon_10.root
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150813_133714/0000/treesTest_NewSmall_10.root file:./SingleMuon_10.root
 echo '### 11 ###'
-gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150804_115350/0000/treesTest_NewSmall_11.root file:./SingleMuon_11.root
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150813_133714/0000/treesTest_NewSmall_11.root file:./SingleMuon_11.root
 echo '### 12 ###'
-gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150804_115350/0000/treesTest_NewSmall_12.root file:./SingleMuon_12.root
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150813_133714/0000/treesTest_NewSmall_12.root file:./SingleMuon_12.root
 echo '### 13 ###'
-gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150804_115350/0000/treesTest_NewSmall_13.root file:./SingleMuon_13.root
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150813_133714/0000/treesTest_NewSmall_13.root file:./SingleMuon_13.root
 echo '### 14 ###'
-gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150804_115350/0000/treesTest_NewSmall_14.root file:./SingleMuon_14.root
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150813_133714/0000/treesTest_NewSmall_14.root file:./SingleMuon_14.root
 echo '### 15 ###'
-gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150804_115350/0000/treesTest_NewSmall_15.root file:./SingleMuon_15.root
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150813_133714/0000/treesTest_NewSmall_15.root file:./SingleMuon_15.root
 echo '### 16 ###'
-gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150804_115350/0000/treesTest_NewSmall_16.root file:./SingleMuon_16.root
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150813_133714/0000/treesTest_NewSmall_16.root file:./SingleMuon_16.root
 echo '### 17 ###'
-gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150804_115350/0000/treesTest_NewSmall_17.root file:./SingleMuon_17.root
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150813_133714/0000/treesTest_NewSmall_17.root file:./SingleMuon_17.root
 echo '### 18 ###'
-gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150804_115350/0000/treesTest_NewSmall_18.root file:./SingleMuon_18.root
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150813_133714/0000/treesTest_NewSmall_18.root file:./SingleMuon_18.root
 echo '### 19 ###'
-gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150804_115350/0000/treesTest_NewSmall_19.root file:./SingleMuon_19.root
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150813_133714/0000/treesTest_NewSmall_19.root file:./SingleMuon_19.root
 echo '### 20 ###'
-gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150804_115350/0000/treesTest_NewSmall_20.root file:./SingleMuon_20.root
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150813_133714/0000/treesTest_NewSmall_20.root file:./SingleMuon_20.root
 hadd -f SingleMuon.root SingleMuon_1.root SingleMuon_2.root SingleMuon_3.root SingleMuon_4.root SingleMuon_5.root SingleMuon_6.root SingleMuon_7.root SingleMuon_8.root SingleMuon_9.root SingleMuon_10.root SingleMuon_11.root SingleMuon_12.root SingleMuon_13.root SingleMuon_14.root SingleMuon_15.root SingleMuon_16.root SingleMuon_17.root SingleMuon_18.root SingleMuon_19.root SingleMuon_20.root 
 
