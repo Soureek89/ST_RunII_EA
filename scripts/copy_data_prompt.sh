a29d5b3a5c891a233315cce863d3f9c1bd857c20
nfaltermann <nfaltermann@users.noreply.github.com>
2015-08-31 09:40:15 +0200

    Update copy_data_prompt.sh

--- scripts/copy_data_prompt.sh
+++ scripts/copy_data_prompt.sh
@@ -38,5 +38,114 @@ echo '### 19 ###'
 gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_19.root file:./SingleMuon_19.root
 echo '### 20 ###'
 gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_20.root file:./SingleMuon_20.root
-hadd -f SingleMuon_prompt.root SingleMuon_1.root SingleMuon_2.root SingleMuon_3.root SingleMuon_4.root SingleMuon_5.root SingleMuon_6.root SingleMuon_7.root SingleMuon_8.root SingleMuon_9.root SingleMuon_10.root SingleMuon_11.root SingleMuon_12.root SingleMuon_13.root SingleMuon_14.root SingleMuon_15.root SingleMuon_16.root SingleMuon_17.root SingleMuon_18.root SingleMuon_19.root SingleMuon_20.root 
-
+echo '### 21 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_21.root file:./SingleMuon_21.root
+echo '### 22 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_22.root file:./SingleMuon_22.root
+echo '### 23 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_23.root file:./SingleMuon_23.root
+echo '### 24 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_24.root file:./SingleMuon_24.root
+echo '### 25 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_25.root file:./SingleMuon_25.root
+echo '### 26 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_26.root file:./SingleMuon_26.root
+echo '### 27 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_27.root file:./SingleMuon_27.root
+echo '### 28 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_28.root file:./SingleMuon_28.root
+echo '### 29 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_29.root file:./SingleMuon_29.root
+echo '### 30 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_30.root file:./SingleMuon_30.root
+echo '### 31 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_31.root file:./SingleMuon_31.root
+echo '### 32 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_32.root file:./SingleMuon_32.root
+echo '### 33 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_33.root file:./SingleMuon_33.root
+echo '### 34 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_34.root file:./SingleMuon_34.root
+echo '### 35 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_35.root file:./SingleMuon_35.root
+echo '### 36 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_36.root file:./SingleMuon_36.root
+echo '### 37 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_37.root file:./SingleMuon_37.root
+echo '### 38 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_38.root file:./SingleMuon_38.root
+echo '### 39 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_39.root file:./SingleMuon_39.root
+echo '### 40 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_40.root file:./SingleMuon_40.root
+echo '### 41 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_41.root file:./SingleMuon_41.root
+echo '### 42 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_42.root file:./SingleMuon_42.root
+echo '### 43 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_43.root file:./SingleMuon_43.root
+echo '### 44 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_44.root file:./SingleMuon_44.root
+echo '### 45 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_45.root file:./SingleMuon_45.root
+echo '### 46 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_46.root file:./SingleMuon_46.root
+echo '### 47 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_47.root file:./SingleMuon_47.root
+echo '### 48 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_48.root file:./SingleMuon_48.root
+echo '### 49 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_49.root file:./SingleMuon_49.root
+echo '### 50 ###'
+gfal-copy srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/nfalterm/single_top/SingleMuon/crab_DATA_tree_prompt/150827_110406/0000/treesTest_NewSmall_50.root file:./SingleMuon_50.root
+hadd -f SingleMuon_prompt.root SingleMuon_1.root SingleMuon_2.root SingleMuon_3.root SingleMuon_4.root SingleMuon_5.root SingleMuon_6.root SingleMuon_7.root SingleMuon_8.root SingleMuon_9.root SingleMuon_10.root SingleMuon_11.root SingleMuon_12.root SingleMuon_13.root SingleMuon_14.root SingleMuon_15.root SingleMuon_16.root SingleMuon_17.root SingleMuon_18.root SingleMuon_19.root SingleMuon_20.root SingleMuon_21.root SingleMuon_22.root SingleMuon_23.root SingleMuon_24.root SingleMuon_25.root SingleMuon_26.root SingleMuon_27.root SingleMuon_28.root SingleMuon_29.root SingleMuon_30.root SingleMuon_31.root SingleMuon_32.root SingleMuon_33.root SingleMuon_34.root SingleMuon_35.root SingleMuon_36.root SingleMuon_37.root SingleMuon_38.root SingleMuon_39.root SingleMuon_40.root SingleMuon_41.root SingleMuon_42.root SingleMuon_43.root SingleMuon_44.root SingleMuon_45.root SingleMuon_46.root SingleMuon_47.root SingleMuon_48.root SingleMuon_49.root SingleMuon_50.root 
+mv SingleMuon_1.root splitted/
+mv SingleMuon_2.root splitted/
+mv SingleMuon_3.root splitted/
+mv SingleMuon_4.root splitted/
+mv SingleMuon_5.root splitted/
+mv SingleMuon_6.root splitted/
+mv SingleMuon_7.root splitted/
+mv SingleMuon_8.root splitted/
+mv SingleMuon_9.root splitted/
+mv SingleMuon_10.root splitted/
+mv SingleMuon_11.root splitted/
+mv SingleMuon_12.root splitted/
+mv SingleMuon_13.root splitted/
+mv SingleMuon_14.root splitted/
+mv SingleMuon_15.root splitted/
+mv SingleMuon_16.root splitted/
+mv SingleMuon_17.root splitted/
+mv SingleMuon_18.root splitted/
+mv SingleMuon_19.root splitted/
+mv SingleMuon_20.root splitted/
+mv SingleMuon_21.root splitted/
+mv SingleMuon_22.root splitted/
+mv SingleMuon_23.root splitted/
+mv SingleMuon_24.root splitted/
+mv SingleMuon_25.root splitted/
+mv SingleMuon_26.root splitted/
+mv SingleMuon_27.root splitted/
+mv SingleMuon_28.root splitted/
+mv SingleMuon_29.root splitted/
+mv SingleMuon_30.root splitted/
+mv SingleMuon_31.root splitted/
+mv SingleMuon_32.root splitted/
+mv SingleMuon_33.root splitted/
+mv SingleMuon_34.root splitted/
+mv SingleMuon_35.root splitted/
+mv SingleMuon_36.root splitted/
+mv SingleMuon_37.root splitted/
+mv SingleMuon_38.root splitted/
+mv SingleMuon_39.root splitted/
+mv SingleMuon_40.root splitted/
+mv SingleMuon_41.root splitted/
+mv SingleMuon_42.root splitted/
+mv SingleMuon_43.root splitted/
+mv SingleMuon_44.root splitted/
+mv SingleMuon_45.root splitted/
+mv SingleMuon_46.root splitted/
+mv SingleMuon_47.root splitted/
+mv SingleMuon_48.root splitted/
+mv SingleMuon_49.root splitted/
+mv SingleMuon_50.root splitted/
